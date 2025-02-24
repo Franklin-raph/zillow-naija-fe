ae3a247f52dbcdc43c5c1cbc6ad0d92af36d12de
franklin-raph <igboekwulusifranklin@gmail.com>
2025-02-24 15:40:17 +0100

    update

--- app/agent/[id]/page.tsx
+++ app/agent/[id]/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/nav-bar/Navbar'
 import Footer from '../../components/footer/Footer'
-import { BiFilter, BiSearch } from 'react-icons/bi'
 import FilterHomeCards from '../../components/filter-home-cards/FilterHomeCards';
 import { CiLocationOn } from 'react-icons/ci';
 
