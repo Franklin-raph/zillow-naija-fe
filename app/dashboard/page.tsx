8c4162339c2dff30a3bb51c2d96261a0d2154f6f
franklin-raph <igboekwulusifranklin@gmail.com>
2025-03-24 12:15:55 +0100

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,11 @@
 
 import React, { useState } from 'react'
 import SideNav from '../components/side-nav/SideNav'
+import TopNav from '../components/top-nav/TopNav'
+import RecentlyPostedHomeCards from "../components/recently-posted-homes-card/RecentlyPostedHomeCards";
+import { LuHouse } from 'react-icons/lu';
+import { PiMoneyWavyLight } from 'react-icons/pi';
+
 
 export default function Page() {
   const [toggleNav, setToggleNav] = useState<boolean>(false)
@@ -10,25 +15,97 @@ export default function Page() {
     setToggleNav(value)
   }
 
+  const properties = [
+    {
+      title: 'Portable 4 Bedroom Bungalow',
+      location: 'Adeniran Ogunsanya Surulere, Lagos',
+      price: '3,500,000',
+      tag: '5 mins Ago',
+      image: "./images/suggested-image.png",
+      details: ['38eds', '28abrooms', '5z7n²'],
+    },
+    {
+      title: 'Luxury 4 Bedroom Duplex',
+      location: 'Ekwulobia-Umunze Road Awka, Anambra',
+      price: '3,500,000',
+      tag: '10 mins Ago',
+      image: "./images/suggested-image-2.png",
+      details: ['38eds', '28abrooms', '5z7n²'],
+    },
+    {
+      title: 'Luxury 4 Bedroom Duplex',
+      location: 'Independence Layout Nza, Enugu',
+      price: '3,500,000',
+      tag: '8 mins Ago',
+      image: "./images/suggested-image-3.png",
+      details: ['38eds', '28abrooms', '5z7n²'],
+    },
+    {
+      title: 'Luxury 4 Bedroom Duplex',
+      location: 'Ekwulobia-Umunze Road Awka, Anambra',
+      price: '3,500,000',
+      tag: '5 mins Ago',
+      image: "./images/suggested-image.png",
+      details: ['38eds', '28abrooms', '5z7n²'],
+    },
+    {
+      title: 'Luxury 4 Bedroom Duplex',
+      location: 'Independence Layout Nza, Enugu',
+      price: '3,500,000',
+      tag: '25 mins Ago',
+      image: "./images/suggested-image-2.png",
+      details: ['38eds', '28abrooms', '5z7n²'],
+    },
+    {
+      title: 'Luxury 4 Bedroom Duplex',
+      location: 'Ekwulobia-Umunze Road Awka, Anambra',
+      price: '3,500,000',
+      tag: 'Yesterday',
+      image: "./images/suggested-image-3.png",
+      details: ['38eds', '28abrooms', '5z7n²'],
+    },
+    {
+      title: 'Luxury 4 Bedroom Duplex',
+      location: 'Independence Layout Nza, Enugu',
+      price: '3,500,000',
+      tag: 'Last week',
+      image: "./images/suggested-image.png",
+      details: ['38eds', '28abrooms', '5z7n²'],
+    },
+  ];
+
   return (
     <div>
-      <div>
-        <SideNav toggle={{
-          toggleNav: toggleNav, 
-          setToggleNav: handleToggleNav
-        }}/>
-        <div className={`w-full lg:w-[78%] ml-auto pb-5 ${toggleNav ? 'lg:ml-[22%]' : 'lg:ml-0'}`}>
-          {/* Optional: Add a hamburger menu button for mobile */}
-          <button 
-            onClick={() => handleToggleNav(true)} 
-            className="lg:hidden block absolute top-4 left-4"
-          >
-            ☰ Menu
-          </button>
-          
-          <div className="">
-            {/* Your page content */}
-          </div>
+      <SideNav toggle={{
+        toggleNav: toggleNav, 
+        setToggleNav: handleToggleNav
+      }}/>
+      <div className={`w-full lg:w-[78%] ml-auto pb-5 ${toggleNav ? 'lg:ml-[22%]' : 'lg:ml-auto'}`}>
+        <TopNav 
+          toggle={{
+            toggleNav: toggleNav,
+            setToggleNav: handleToggleNav
+          }}
+          pageTitle={'Dashboard'}
+        />
+        <div className='mt-8'>
+          <section className="w-[95%] mx-auto md:px-[1rem] px-[0px] pb-[80px]">
+            <p className="font-[#212121] font-[700] md:text-[25px] text-[18px] mb-3">Dashboard</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 border-b pb-5'>
+              <div className='bg-[#F9F9F9] p-5 rounded-[10px]'>
+                <LuHouse className='text-[30px] mb-5'/>
+                <p className='text-[18px] text-[#212121] font-[500]'> <span>Total Properties:</span> 123</p>
+              </div>
+              <div className='bg-[#F9F9F9] p-5 rounded-[10px]'>
+                <PiMoneyWavyLight className='text-[30px] mb-5'/>
+                <p className='text-[18px] text-[#212121] font-[500]'> <span>Total Properties Sold:</span> 123</p>
+              </div>
+              <div className='bg-[#F9F9F9] p-5 rounded-[10px]'>
+                <LuHouse className='text-[30px] mb-5'/>
+                <p className='text-[18px] text-[#212121] font-[500]'> <span>Total Properties:</span> 123</p>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
